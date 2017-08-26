fd93e1c864a9bd1040107d6213431776ac1015c9
mariomelchor <mario@mariomelchor.com>
2017-08-25 23:01:53 -0700

    Basic Card method appending to basic.json

--- app.js
+++ app.js
@@ -1,3 +1,43 @@
-// Required Files
+// Variables Required Fields
 var basicFlashcard = require('./basic-card.js');
+var inquirer = require('inquirer');
 var fs = require('fs');
+
+// Initial Question on what to do.
+inquirer.prompt([{
+  name: 'task',
+  message: 'What would you like to do?',
+  type: 'list',
+  choices: [ { name: 'Add Card' }, { name: 'Show Cards' }]
+}]).then(function(answer) {
+    if ( answer.task === 'Add Card' ) {
+      console.log('Add Card');
+        addCard();
+    } else if ( answer.task === 'Show Cards' ) {
+      console.log('Show Card');
+        showCards();
+    }
+});
+
+function addCard() {
+
+  inquirer.prompt([{
+      name: 'front',
+      message: 'What is the question?'
+    }, {
+      name: 'back',
+      message: 'What is the answer?'
+   }]).then(function(answers) {
+
+    var newQuestion = new basicFlashcard( answers.front,  answers.back );
+    newQuestion.card();
+
+    // Run the add Card question again
+    addCard();
+  });
+
+}
+
+function showCards() {
+
+}
--- basic-card.js
+++ basic-card.js
@@ -1,22 +1,24 @@
 module.exports = BasicCard;
+var fs = require('fs');
 
 function BasicCard( front, back ) {
   this.front = front;
   this.back = back;
   this.card = function() {
 
-  var question = {
+    // Setup variable
+    var question = {
       front: this.front,
       back: this.back,
-  };
+    };
 
-  // add card to log.txt
-  fs.appendFile("basic.json", JSON.stringify(question) + ';', "utf8", function(error) {
-    // if there is an error, log the error
-    if (error) {
+    // add question variable to basic.json
+    fs.appendFile("basic.json", JSON.stringify( question, null, 2) + ',', "utf8", function(error) {
+      // if there is an error, log the error
+      if (error) {
         console.log(error);
-    }
-  });
+      }
+    });
 
   };
 }
